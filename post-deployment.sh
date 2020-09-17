50633e994b12d287f6df23810107ac35ad68ee9f
shumatep <pshumate@easydynamics.com>
2020-09-17 15:55:03 -0400

    change service name and url

--- post-deployment.sh
+++ post-deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cf ssh -N -T -L 8081:localhost:8081 give-api-gateway &
-curl -i -X POST http://localhost:8081/services --data name='usps' --data url='https://give-ipp-usps-intelligent-hyena-tl.app.cloud.gov/' &
-curl -X POST http://localhost:8081/plugins/ --data "name=key-auth" 
+cf ssh -N -T -L 8001:localhost:8001 give-api-gateway &
+curl -i -X POST http://localhost:8001/services --data name='usps' --data url='http://give-ipp-usps-intelligent-hyena-tl.app.cloud.gov' &
+curl -X POST http://localhost:8001/plugins/ --data "name=key-auth" 
 kill $!
